7d43af0e58be002b5f046e280877d2aa34d3c694
Aleksey Fedorov <easy.mail.to.copy@gmail.com>
2021-06-03 01:16:15 +0300

    Fix side effect in Users component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,23 +4,30 @@ import * as axios from "axios";
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(u => <div key={u.id}>
                 <div>
                     <div><img src={u.photos.small != null ? u.photos.small : lion} className={s.userPhoto}/></div>
                     <div>
                         {
                             u.followed
-                            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                            : <button onClick={() => {props.follow(u.id)}}>Follow</button>
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }}>Follow</button>
                         }
                     </div>
                 </div>
