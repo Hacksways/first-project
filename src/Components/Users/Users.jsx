b47b27402a002baf0b4ac358bb86c4a0a7341cb2
Aleksey Fedorov <easy.mail.to.copy@gmail.com>
2021-06-06 22:46:42 +0300

    Create and learn componentDidMount

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,15 +5,13 @@ import * as axios from "axios";
 
 class Users extends React.Component {
 
-    constructor (props){
-    super(props);
+    componentDidMount() {
         if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 this.props.setUsers(response.data.items);
             });
         }
     }
-
     render() {
         return (
             <div>
